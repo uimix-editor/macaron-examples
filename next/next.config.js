befa31a4329206077607d6e766edefee12827fcc
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-22 20:21:27 +0900

    Add webpack config

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.macaron/,
+      use: [options.defaultLoaders.babel, "@macaron-elements/loader-webpack"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
